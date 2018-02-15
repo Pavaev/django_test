20c3f991811bf87e1342d08ca2362ea647f45d98
Panaev <28530@mail.ru>
2018-02-15 13:41:10 +0300

    Add static files

--- article/urls.py
+++ article/urls.py
@@ -5,10 +5,6 @@ from . import views
 app_name = 'article'
 
 urlpatterns = [
-    re_path(r'^1/', views.basic_one),
-    re_path(r'^2/', views.template_two),
-    re_path(r'^3/', views.template_three_simple),
-    re_path(r'^articles/all/$', views.articles),
-    re_path(r'articles/get/(?P<article_id>\d+)/$', views.article),
-    re_path(r'^$', views.articles),
+    re_path(r'articles/get/(?P<article_id>\d+)/$', views.article, name="article"),
+    re_path(r'^$', views.articles, name='articles'),
 ]
